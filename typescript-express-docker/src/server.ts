e432b197bd2f4d37b0fd79b40cd3ab3270da75db
Shlomi Elbaz <shlomisderot@gmail.com>
2022-02-15 12:14:05 +0200

    Add files via upload

--- typescript-express-docker/src/server.ts
+++ typescript-express-docker/src/server.ts
@@ -1,1 +1,9 @@
+import express, { Request, Response } from 'express';
 
+const server = express();
+
+server.get('/healthcheck',(_req: Request, res: Response)=>{
+    res.sendStatus(200);
+})
+
+export { server }
